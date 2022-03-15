f23315e0321e837045579d1ab44186d04dd93ed4
Mellie Dee <89175650+MellieDee@users.noreply.github.com>
2022-03-14 23:15:20 -0700

    add eventDate var to Events list component

--- client/src/components/EventList.js
+++ client/src/components/EventList.js
@@ -88,7 +88,7 @@ const EventList = () => {
                                         </div>
                                         <ul className="list-group list-group-flush">
                                             <li className="list-group-item"><span className="material-icons adjust-icons">place</span> {event.city}</li>
-                                            <li className="list-group-item"><span className="material-icons adjust-icons color-two">today</span> March 20th, 2022</li>
+                                            <li className="list-group-item"><span className="material-icons adjust-icons color-two">today</span> {event.eventDate}</li>
 
                                             <Link to={`/profiles/${event.host}`}>
                                                 <li className="list-group-item">Created by: {event.host}</li>
